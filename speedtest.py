a34a15299fa2e7e82fa720b7a5091c482b2d468c
Ankit Raj <40168521+raazankeet@users.noreply.github.com>
2022-11-08 01:43:38 +0530

    webhooks changes
    Changes:
    Webhook usage
    Regular expression removed
    Json response from speedtest for better extraction
    More data points captured

--- speedtest.py
+++ speedtest.py
@@ -5,22 +5,54 @@ import time
 from influxdb import InfluxDBClient
 import random
 import sys
-from Adafruit_IO import MQTTClient
+import requests
+import json
 
-ADAFRUIT_IO_USERNAME = '******'
-ADAFRUIT_IO_KEY = '********'
 
-response = subprocess.Popen('/usr/bin/speedtest --accept-license --accept-gdpr', shell=True, stdout=subprocess.PIPE).stdout.read().decode('utf-8')
 
-ping = re.search('Latency:\s+(.*?)\s', response, re.MULTILINE)
-download = re.search('Download:\s+(.*?)\s', response, re.MULTILINE)
-upload = re.search('Upload:\s+(.*?)\s', response, re.MULTILINE)
-jitter = re.search('\((.*?)\s.+jitter\)\s', response, re.MULTILINE)
+response = subprocess.Popen('/usr/bin/speedtest --accept-license --accept-gdpr -f json -u bps', shell=True, stdout=subprocess.PIPE).stdout.read().decode('utf-8')
 
-ping = ping.group(1)
-download = download.group(1)
-upload = upload.group(1)
-jitter = jitter.group(1)
+json_response = json.loads(response)
+
+# ping = re.search('Latency:\s+(.*?)\s', response, re.MULTILINE)
+# download = re.search('Download:\s+(.*?)\s', response, re.MULTILINE)
+# upload = re.search('Upload:\s+(.*?)\s', response, re.MULTILINE)
+# jitter = re.search('\((.*?)\s.+(jitter:)\)\s', response, re.MULTILINE)
+
+# Typical response:
+# {"type":"result","timestamp":"2022-08-17T18:32:59Z","ping":{"jitter":0.492,"latency":2.456,"low":2.308,"high":3.372},"download":{"bandwidth":11741893,"bytes":91658880,"elapsed":7807,"latency":{"iqm":376.667,"low":6.338,"high":542.037,"jitter":80.576}},"upload":{"bandwidth":11712337,"bytes":42328800,"elapsed":3614,"latency":{"iqm":6.528,"low":3.674,"high":7.725,"jitter":0.571}},"packetLoss":0,"isp":"Airtel Broadband","interface":{"internalIp":"192.168.1.108","name":"eth0","macAddr":"B8:27:EB:90:AD:1B","isVpn":false,"externalIp":"106.215.80.93"},"server":{"id":18996,"host":"speedtestdel.airtelbroadband.in","port":8080,"name":"Airtel Broadband","location":"Delhi","country":"India","ip":"122.160.129.138"},"result":{"id":"9819e091-b129-49c4-8d15-e5a50d6f82a5","url":"https://www.speedtest.net/result/c/9819e091-b129-49c4-8d15-e5a50d6f82a5","persisted":true}}
+#print(response)
+
+
+
+ping = format(round(json_response["ping"]["latency"],2),".2f")
+download = format(round(json_response["download"]["bandwidth"]/125000,2),".2f")
+upload = format(round(json_response["upload"]["bandwidth"]/125000,2),".2f")
+jitter = format(round(json_response["ping"]["jitter"],2),".2f")
+
+isp = json_response["isp"]
+
+packet_loss = json_response["packetLoss"]
+
+internal_ip = json_response["interface"]["internalIp"]
+external_ip = json_response["interface"]["externalIp"]
+
+server_host = json_response["server"]["host"]
+server_port = json_response["server"]["port"]
+server_name = json_response["server"]["name"]
+server_location = json_response["server"]["location"]
+server_country = json_response["server"]["country"]
+server_ip = json_response["server"]["ip"]
+
+result_url = json_response["result"]["url"]
+
+#print(ping)
+#print(download)
+#print(upload)
+#print(jitter)
+
+#sys.exit("Debugging...")   
+
 
 speed_data = [
     {
@@ -38,13 +70,12 @@ speed_data = [
 ]
 
 
+file1 = open("/home/ankit/speedtest/results3.csv", "a+")  # append mode
 
-file1 = open("/home/ankit/speedtest/results2.csv", "a+")  # append mode
+if os.stat("/home/ankit/speedtest/results3.csv").st_size == 0:
+    file1.write('Date,Time,Ping (ms),Jitter (ms),Download (Mbps),Upload (Mbps),ISP,Packet_Loss,Internal_IP,External_IP,Server_Host,Server_Port,Server_Name,Server_Location,Server_Country,Server_IP,Results_URL\r\n')
 
-if os.stat("/home/ankit/speedtest/results2.csv").st_size == 0:
-    file1.write('Date,Time,Ping (ms),Jitter (ms),Download (Mbps),Upload (Mbps)\r\n')
-
-file1.write('{},{},{},{},{},{}\r\n'.format(time.strftime('%m/%d/%y'), time.strftime('%H:%M'), ping, jitter, download, upload))
+file1.write('{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{}\r\n'.format(time.strftime('%m/%d/%y'), time.strftime('%H:%M'), ping, jitter, download, upload,isp,packet_loss,internal_ip,external_ip,server_host,server_port,server_name,server_location,server_country,server_ip,result_url))
 file1.close()
 
 
@@ -52,40 +83,23 @@ client = InfluxDBClient('localhost', 8086, 'speedmonitor', 'pimylifeup', 'intern
 client.write_points(speed_data)
 
 
-#client = InfluxDBClient('localhost', 8086, 'speedmonitor', 'pimylifeup', 'internetspeed')
-#client.write_points(speed_data)
+# Feeds
+
+download_url="https://io.adafruit.com/api/v2/webhooks/feed/********"
+jitter_url="https://io.adafruit.com/api/v2/webhooks/feed/********"
+ping_url="https://io.adafruit.com/api/v2/webhooks/feed/********"
+upload_url="https://io.adafruit.com/api/v2/webhooks/feed/********"
 
 
 
+download_rate = {'value':download}
+upload_rate = {'value':upload}
+jitter_rate = {'value':jitter}
+ping_rate = {'value':ping}
 
-def connected(client):
-    client.subscribe('speedtest.upload_rate_feed')
+session = requests.Session()
 
-def disconnected(client):
-    print('Disconnected from Adafruit IO!')
-    sys.exit(1)
-
-def message(client, feed_id, payload):
-    pass
-
-client = MQTTClient(ADAFRUIT_IO_USERNAME, ADAFRUIT_IO_KEY)
-
-
-client.on_connect    = connected
-client.on_disconnect = disconnected
-client.on_message    = message
-
-
-client.connect()
-client.loop_background()
-
-
-client.publish('speedtest.upload_rate_feed', upload)
-client.publish('speedtest.download_rate_feed', download)
-client.publish('speedtest.ping_feed', ping)
-client.publish('speedtest.jitter_feed', jitter)
-print ("Message sent, exiting now!")
-
-
-
-
+session.post(download_url,data=download_rate)
+session.post(upload_url,data=upload_rate)
+session.post(jitter_url,data=jitter_rate)
+session.post(ping_url,data=ping_rate)
